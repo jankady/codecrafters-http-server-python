fb697d5fc4c6f40c69c9a9a3d91f39d11fe3bfdb
jankady <kaduch05@gmail.com>
2025-10-21 22:19:28 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,7 +6,28 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    conn, addr = server_socket.accept() # wait for client
+
+    data = conn.recv(1024).decode().strip().split(" ")
+
+    http_method = data[0]
+    http_path = data[1]
+    http_version = data[2].split("\r\n")[0]
+
+
+    response = ""
+    match http_method:
+        case "GET":
+            response = (
+                f"{http_version} 200 OK\r\n\r\n"
+
+            )
+        case "POST":
+            pass
+
+    conn.send(response.encode())
+    conn.close()
+
 
 
 if __name__ == "__main__":
